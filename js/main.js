48f33ab5efa034ab40b64388524fc6a90b53ee50
James Fargher <proglottis@gmail.com>
2013-10-23 20:05:48 +1300

    Scenario 2

--- js/main.js
+++ js/main.js
@@ -5,18 +5,65 @@ function ControlScenarioCtrl($scope) {
   $scope.total = 0.0;
 
   $scope.keep = function() {
-    $scope.payout = $scope.amount;
+    $scope.setEnvelopes()
+    $scope.payout = $scope.envelope1;
     $scope.updateTotal();
   }
 
   $scope.swap = function() {
-    $scope.payout = $scope.amount * 2 * Math.floor(Math.random() * 2);
+    $scope.setEnvelopes()
+    $scope.payout = $scope.envelope2;
     $scope.updateTotal();
   }
 
   $scope.updateTotal = function() {
     $scope.count += 1;
     $scope.total += $scope.payout;
+  }
+
+  $scope.setEnvelopes = function() {
+    $scope.envelope1 = $scope.amount;
+    $scope.envelope2 = $scope.amount * 2 * Math.floor(Math.random() * 2);
+  }
+
+  $scope.reset = function() {
+    $scope.count = 0;
+    $scope.payout = 0.0;
+    $scope.total = 0.0;
+  }
+}
+
+function ClassicScenarioCtrl($scope) {
+  $scope.amount = 100.0;
+  $scope.count = 0;
+  $scope.payout = 0.0;
+  $scope.total = 0.0;
+
+  $scope.keep = function() {
+    $scope.setEnvelopes()
+    $scope.payout = $scope.envelope1;
+    $scope.updateTotal();
+  }
+
+  $scope.swap = function() {
+    $scope.setEnvelopes()
+    $scope.payout = $scope.envelope2;
+    $scope.updateTotal();
+  }
+
+  $scope.updateTotal = function() {
+    $scope.count += 1;
+    $scope.total += $scope.payout;
+  }
+
+  $scope.setEnvelopes = function() {
+    if(Math.floor(Math.random() * 2)) {
+      $scope.envelope1 = $scope.amount;
+      $scope.envelope2 = $scope.amount * 2;
+    } else {
+      $scope.envelope1 = $scope.amount * 2;
+      $scope.envelope2 = $scope.amount;
+    }
   }
 
   $scope.reset = function() {
